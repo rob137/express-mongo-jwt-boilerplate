27fdfe19434904397d9d6862eac8a1f87919f5d2
rob <robertaxelkirby@gmail.com>
2019-01-15 16:33:07 +0000

    Add cors to server.js, plus minor refactor

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-port = 3000;
-
+app.use(cors());
 app.get('/', (req, res) => res.send('Hello world!'));
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
